a4eb38fc5311fb63f6967d79d7b9d154b788949a
Alexander <alexanderacosta2286@gmail.com>
2024-03-20 19:05:59 -0500

    adding changes

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 console.log('hola CIAF 2024 APP file')
+console.log('hola CIAF 2024')
+
+let counter = 1000;
+
+//suma
+counter = counter + 20;
+console.log(counter)
+counter += 30;
+console.log(counter)
+//resta
+counter = counter -20;
+console.log(counter)
+counter -= 30;
+console.log(counter) 
+//multiplicacion
+counter = counter * 3;
+console.log(counter)
+counter *= 4;
+console.log(counter)
+//division
+counter = counter / 5;
+console.log(counter)
+counter /= 4;
+console.log(counter)
+//resuido
+counter = counter % 3;
+console.log(counter)
+counter %= 3;
+console.log(counter)
+//exponenciacion
+counter = counter ** 3;
+console.log(counter)
+counter **= 2;
+console.log(counter);
+
+let amountOne = "100";
+let amountTwo = 100;
+
+let result = amountOne == amountTwo;
+console.log(result)
+
+let result2 = amountOne === amountTwo;
+console.log(result2)
+
